a4144b94f983bf296218265ec181fef4629d8565
Mehranalam <mehranalambeigi@yahoo.com>
2021-08-10 16:15:22 +0430

    change data

--- deploy.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-version = sys.argv[1]
-name = sys.argv[2]
-
-os.system(f"fandogh image publish --version v{version}")
-os.system(f"fandogh service deploy --version v{version} --image {name} --name {name}")
